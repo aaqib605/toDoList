d6679739b5a2f447f1070b717993c6fe64a81fca
aaqib <aaqibjavaid605@gmail.com>
2023-04-26 20:11:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const toDoList = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+
+  const day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, toDoList: toDoList });
+});
+
+app.post("/", (req, res) => {
+  const toDoItem = req.body.item;
+  toDoList.push(toDoItem);
+
+  res.redirect("/");
+});
+
+app.listen(5050, console.log("Port listening on port 5050"));
